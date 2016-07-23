f147ab669a7413f6273b8bddff5c8bad2f68675f
Guillermo Esteves <g@gesteves.com>
2016-07-23 15:01:33 -0400

    Better help message

--- app.rb
+++ app.rb
@@ -47,8 +47,8 @@ end
 post '/weather' do
   if params[:token] == ENV['SLACK_VERIFICATION_TOKEN']
     query = params[:text].sub(/^\s*(in|for|at)\s+/, '').strip
-    if query == ''
-      response = { text: 'You have to enter a location, like `/weather in 1600 Pennsylvania Avenue NW, Washington, DC`. You can enter just a city or zip code, or a full address. You can also specify if you want your results in celsius, like `/weather in new york in celsius`.', response_type: 'ephemeral' }.to_json
+    if query == '' || query == 'help'
+      response = { text: 'Enter a location to get the current weather forecast for it. You can enter just a city or zip code, or a full address. For example, `/weather in 1600 Pennsylvania Avenue NW, Washington, DC`, `/weather in washington, dc`, or `/weather in 20036`. You can also specify if you want your results in celsius, like `/weather in new york in celsius`.', response_type: 'ephemeral' }.to_json
     else
       response = $cache.get(parameterize(query))
       if response.nil?
